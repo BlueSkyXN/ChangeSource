69dcf3d227f727445d847f3c31178683e8a698f9
BlueSkyXN <xienan0714@gmail.com>
2021-02-27 19:01:50 +0800

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-wget -N --no-check-certificate "https://raw.githubusercontent.com/BlueSkyXN/ChangeSource/master/box.sh" && chmod +x box.sh && ./box.sh
+wget -N --no-check-certificate "https://raw.githubusercontent.com/BlueSkyXN/ChangeSource/master/box.sh" 
+
+chmod +x box.sh 
+
+ ./box.sh
