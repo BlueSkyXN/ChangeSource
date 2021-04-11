6b63cd9e3fc6b02dbb009299438052afdb817997
BlueSkyXN <xienan0714@gmail.com>
2021-04-11 18:38:00 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
-sudo yum -y install curl wget epel-release unzip screen fuse fuse-devel cpulimit
-sudo yum -y update
-sudo bash <(curl -Ls "https://cdn.000714.xyz/BlueSkyXN/ChangeSource/master/clone.sh")
-sudo wget -O "/root/speedtest" "https://objectstorage.me-dubai-1.oraclecloud.com/n/axdoh51w1xdx/b/GIT/o/speedtest" --no-check-certificate -T 30 -t 5 -d
-sudo chmod +x "/root/speedtest"
-sudo chmod 777 "/root/speedtest"
-sudo wget -O "/root/besttrace" "https://objectstorage.me-dubai-1.oraclecloud.com/n/axdoh51w1xdx/b/GIT/o/besttrace" --no-check-certificate -T 30 -t 5 -d
-sudo chmod +x "/root/besttrace"
-sudo chmod 777 "/root/besttrace"
-sudo wget -O "/root/tcp.sh" "https://cdn.000714.xyz/BlueSkyXN/ChangeSource/master/tcp.sh" --no-check-certificate -T 30 -t 5 -d
-sudo chmod +x "/root/tcp.sh"
-sudo chmod 777 "/root/tcp.sh"
-sudo wget -O "/root/mt.sh" "https://cdn.000714.xyz/BlueSkyXN/ChangeSource/master/mt.sh" --no-check-certificate -T 30 -t 5 -d
-sudo chmod +x "/root/mt.sh"
-sudo chmod 777 "/root/mt.sh"
+yum -y install curl wget epel-release unzip screen fuse fuse-devel cpulimit
+yum -y update
+bash <(curl -Ls "https://cdn.000714.xyz/BlueSkyXN/ChangeSource/master/clone.sh")
+wget -O "/root/speedtest" "https://objectstorage.me-dubai-1.oraclecloud.com/n/axdoh51w1xdx/b/GIT/o/speedtest" --no-check-certificate -T 30 -t 5 -d
+chmod +x "/root/speedtest"
+chmod 777 "/root/speedtest"
+wget -O "/root/besttrace" "https://objectstorage.me-dubai-1.oraclecloud.com/n/axdoh51w1xdx/b/GIT/o/besttrace" --no-check-certificate -T 30 -t 5 -d
+chmod +x "/root/besttrace"
+chmod 777 "/root/besttrace"
+wget -O "/root/tcp.sh" "https://cdn.000714.xyz/BlueSkyXN/ChangeSource/master/tcp.sh" --no-check-certificate -T 30 -t 5 -d
+chmod +x "/root/tcp.sh"
+chmod 777 "/root/tcp.sh"
+wget -O "/root/mt.sh" "https://cdn.000714.xyz/BlueSkyXN/ChangeSource/master/mt.sh" --no-check-certificate -T 30 -t 5 -d
+chmod +x "/root/mt.sh"
+chmod 777 "/root/mt.sh"
 clear
