2742b590caed4067e0ab9e688eca12f44bf214cc
BlueSkyXN <xienan0714@gmail.com>
2021-04-11 14:32:32 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+yum -y install curl wget epel-release unzip screen fuse fuse-devel cpulimit
+yum -y update
+sudo bash <(curl -Ls "https://cdn.000714.xyz/BlueSkyXN/ChangeSource/master/clone.sh")
+wget -O "/root/speedtest" "https://cdn.000714.xyz/BlueSkyXN/ChangeSource/master/speedtest" --no-check-certificate -T 30 -t 5 -d
+chmod +x "/root/speedtest"
+chmod 777 "/root/speedtest"
+wget -O "/root/besttrace" "https://cdn.000714.xyz/BlueSkyXN/ChangeSource/master/besttrace" --no-check-certificate -T 30 -t 5 -d
+chmod +x "/root/besttrace"
+chmod 777 "/root/besttrace"
+wget -O "/root/tcp.sh" "https://cdn.000714.xyz/BlueSkyXN/ChangeSource/master/tcp.sh" --no-check-certificate -T 30 -t 5 -d
+chmod +x "/root/tcp.sh"
+chmod 777 "/root/tcp.sh"
+wget -O "/root/mt.sh" "https://cdn.000714.xyz/BlueSkyXN/ChangeSource/master/mt.sh" --no-check-certificate -T 30 -t 5 -d
+chmod +x "/root/mt.sh"
+chmod 777 "/root/mt.sh"
+clear
