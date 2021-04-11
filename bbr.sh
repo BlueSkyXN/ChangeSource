665e99cf450d79e12d3f9a21ce1fe8fa9fe40eae
BlueSkyXN <xienan0714@gmail.com>
2021-04-11 22:43:18 +0800

    Create bbr.sh

---
+++ bbr.sh
@@ -0,0 +1,4 @@
+rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
+yum --enablerepo=elrepo-kernel -y install kernel-ml kernel-ml-devel
+grub2-set-default 0
