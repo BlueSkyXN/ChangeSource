a42a367c84f31ed6d498ee92bc206be1c54da0c2
BlueSkyXN <xienan0714@gmail.com>
2021-04-18 14:48:43 +0800

    Update box.sh

--- box.sh
+++ box.sh
@@ -359,7 +359,6 @@ function start_menu(){
     red " BlueSkyXN  综合工具箱" 
     red " CentOS·YUM Supported ONLY" 
     yellow " =================================================="
-    echo
     green " 1. IPV.SH ipv4/6优先级调整一键脚本·下载" 
     green " 2. IPT.SH iptable一键脚本"
     green " 3. SpeedTest-Linux 下载"
@@ -367,33 +366,25 @@ function start_menu(){
     green " 5. ChangeSource Linux换源脚本·下载"
     green " 6. Besttrace 路由追踪·下载"
     green " 7. NEZHA.SH哪吒面板/探针·下载"
-    echo
     yellow " --------------------------------------------------"
-    echo
     green " 11. 获取本机IP"
     green " 12. 安装最新BBR内核·使用YUM" 
     green " 13. 启动BBR FQ算法"
     green " 14. 系统网络配置优化"
     green " 15. Git 新版 安装"
     green " 16. 宝塔面板 自动磁盘挂载工具"
-    echo
     yellow " --------------------------------------------------"
-    echo
     green " 21. Superbench 综合测试"
     green " 22. MT.SH 流媒体解锁测试"
     green " 23. Lemonbench 综合测试"
     green " 24. UNIXbench 综合测试"
     green " 25. 三网Speedtest测速"
     green " 26. Memorytest 内存压力测试"
-    echo
     yellow " --------------------------------------------------"
-    echo
     green " 31. MTP&TLS 一键脚本"
     green " 32. V2UI 一键脚本"
     green " 33. Aria2 最强安装与管理脚本"
-    echo
     yellow " --------------------------------------------------"
-    echo
     green " 41. 宝塔面板 官方版·一键安装"
     green " 42. 宝塔面板 官方版·一键更新"
     green " 43. 宝塔面板 5.9开源免费版·一键安装"
@@ -401,7 +392,6 @@ function start_menu(){
     green " 45. 宝塔面板 Hostcli 破解版·一键转移"
     green " 46. 莉塔面板·一键安装（安装后需要更新一下）"
     green " 47. 莉塔面板·一键更新（安装后需要更新一下）"
-    echo
     green " =================================================="
     green " 0. 退出脚本"
     echo
