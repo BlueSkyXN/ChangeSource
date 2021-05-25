243c382906705c84d3e18d647d9aa061dfeec478
BlueSkyXN <xienan0714@gmail.com>
2021-05-25 11:52:57 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -4,6 +4,6 @@ export PATH
 
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
-yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
 yum --enablerepo=elrepo-kernel -y install kernel-ml kernel-ml-devel
 grub2-set-default 0
