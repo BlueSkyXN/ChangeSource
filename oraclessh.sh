0aec969c49ca8d9214d8aa68f4ebcf38da3692c7
BlueSkyXN <xienan0714@gmail.com>
2021-05-30 11:14:21 +0800

    3.8

---
+++ oraclessh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# https://hostloc.com/thread-851164-1-1.html
+echo root:password12345678 |sudo chpasswd root
+sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+sudo service sshd restart
